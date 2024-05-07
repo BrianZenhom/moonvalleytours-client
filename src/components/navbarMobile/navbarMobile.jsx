7ba334560810497e568527291e9027c744ef2d4e
brianzenhom <brianzenhom@hotmail.com>
2024-05-07 10:46:33 +0200

    Add Navbar Mobile

--- src/components/navbarMobile/navbarMobile.jsx
+++ src/components/navbarMobile/navbarMobile.jsx
@@ -6,10 +6,24 @@ import User from '../../assets/icons/User'
 import Help from '../../assets/icons/Help'
 import Menu from '../../assets/icons/Menu'
 import Close from '../../assets/icons/Close'
+import Cart from '../../assets/icons/Cart'
+import SeePw from '../../assets/icons/SeePw'
+import HidePw from '../../assets/icons/HidePw'
+import LeftBack from '../../assets/icons/LeftBack'
 
 const NavbarMobile = () => {
   const [menu, setMenu] = useState(false)
   const [loginOpen, setLoginOpen] = useState(false)
+  const [visible, setVisible] = useState(false)
+  const [remember, setRemember] = useState(false)
+
+  const handleRemember = () => {
+    setRemember(!remember)
+  }
+
+  const handleHiddenPw = () => {
+    setVisible(!visible)
+  }
 
   const handleLoginOpen = () => {
     setLoginOpen(!loginOpen)
@@ -17,13 +31,61 @@ const NavbarMobile = () => {
 
   const handleOpen = () => {
     setMenu(!menu)
+    setLoginOpen(false)
   }
-
-  console.log(loginOpen)
 
   return (
     <>
-      <div className="navbarmobile">
+      <aside className="login_mobile">
+        <div
+          className={
+            loginOpen
+              ? 'login_mobile-fullscreen login_visible'
+              : 'login_mobile-fullscreen'
+          }
+        >
+          <div className="header_wrapper-nav">
+            <button onClick={handleLoginOpen}>
+              <LeftBack />
+            </button>
+            <span>My Account</span>
+            <span>
+              <Cart />
+            </span>
+          </div>
+          <div className="login_content-container">
+            <span>Have an account? Access your account.</span>
+            <form className="login_mobile-form" action="">
+              <div className="form_content">
+                <div className="form_inputs">
+                  <input type="text" placeholder="User or email" />
+                  <div className="password_input">
+                    <input
+                      type={visible ? 'text' : 'password'}
+                      placeholder="Password"
+                    />
+                    <div className="hideshow" onClick={handleHiddenPw}>
+                      {visible ? <SeePw /> : <HidePw />}
+                    </div>
+                  </div>
+                </div>
+                <div onClick={handleRemember} className="rememberme">
+                  Remember me {remember ? 'check' : 'unchecked'}
+                </div>
+              </div>
+              <button aria-label="submit login">Login</button>
+            </form>
+            <div className="forgot_password">
+              <span>I&apos;ve forgotten my password</span>
+            </div>
+            <div className="orlogin">
+              <span>or login with</span>
+              <div className="socials">Google Facebook Apple</div>
+            </div>
+          </div>
+        </div>
+      </aside>
+      <nav className="navbarmobile">
         <Logo type={menu ? 'logonav logonavLeft' : 'logonav'} />
         <div className={menu ? 'button' : ' '}>
           <button onClick={handleOpen}>{menu ? <Close /> : <Menu />}</button>
@@ -57,7 +119,7 @@ const NavbarMobile = () => {
             </ul>
           </nav>
         </div>
-      </div>
+      </nav>
     </>
   )
 }
