f16a2562d220b2630e4aee501d398b932b7be772
brianzenhom <brianzenhom@hotmail.com>
2024-05-07 10:45:59 +0200

    Skeleton

---
+++ src/components/CardSkeleton/CardSkeleton.jsx
@@ -0,0 +1,11 @@
+import Skeleton from 'react-loading-skeleton'
+
+const CardSkeleton = () => {
+  return (
+    <div className="card-skeleton">
+      <Skeleton />
+    </div>
+  )
+}
+
+export default CardSkeleton
