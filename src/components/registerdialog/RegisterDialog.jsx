012f83dbe25cdaf02a8c5f4a0c6e523dfe77a240
brianzenhom <brianzenhom@hotmail.com>
2024-05-29 01:58:16 +0200

    Add register login icons

--- src/components/registerdialog/RegisterDialog.jsx
+++ src/components/registerdialog/RegisterDialog.jsx
@@ -1,5 +1,7 @@
 import CloseModal from '../../assets/icons/CloseModal'
 import RegisterForm from '../registerform/RegisterForm'
+import SocialRegister from '../socialRegister/socialRegister'
+import { Apple, Facebook, Google } from '../../assets/icons/SocialsRegistration'
 import PropTypes from 'prop-types'
 
 const RegisterDialog = ({ setLoginOpen, setMenu, dialogRef }) => {
@@ -15,17 +17,27 @@ const RegisterDialog = ({ setLoginOpen, setMenu, dialogRef }) => {
     <>
       <dialog ref={dialogRef} className="registerForm">
         <div className="registerForm_content">
-          <div className="registerForm_title">
-            <div className="titleRegistration">Create an account</div>
-            <div onClick={toggleCloseDialog}>
-              <CloseModal />
+          <div className="registerForm_wrapper">
+            <div className="registerForm_title">
+              <div className="titleRegistration">Create an account</div>
+              <div onClick={toggleCloseDialog}>
+                <CloseModal />
+              </div>
+            </div>
+            <RegisterForm
+              setMenu={setMenu}
+              toggleCloseDialog={toggleCloseDialog}
+              setLoginOpen={setLoginOpen}
+            />
+            <div className="registerForm_alternatives">
+              <span>or register with</span>
+              <div className="registerForm_socials">
+                <SocialRegister icon={<Facebook />} social="Facebook" />
+                <SocialRegister icon={<Google />} social="Google" />
+                <SocialRegister icon={<Apple />} social="Apple" />
+              </div>
             </div>
           </div>
-          <RegisterForm
-            setMenu={setMenu}
-            toggleCloseDialog={toggleCloseDialog}
-            setLoginOpen={setLoginOpen}
-          />
           <small>
             By creating an account, you agree to MOONVALLEYTOURS, S.L.
             (MoonValleyTours) processing your data for service provision and
