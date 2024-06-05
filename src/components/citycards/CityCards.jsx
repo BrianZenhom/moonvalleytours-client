829b295af8cb029486e5812f5f921887c26c0aa1
brianzenhom <brianzenhom@hotmail.com>
2024-06-05 13:31:19 +0200

    Add Best Cards

--- src/components/citycards/CityCards.jsx
+++ src/components/citycards/CityCards.jsx
@@ -4,44 +4,46 @@ import img from '../../assets/images/image(3).webp'
 
 export default function CityCards({ item }) {
   return (
-    <div className="city_card">
-      <div className="city_card-wrapper">
-        <img src={img} alt="" />
-        <div className="city_card-content">
-          <div className="city_card-title">
-            <h3>{item.title}</h3>
-            <div className="city_card-details">
-              <strong>{item.ratingsAverage}/5</strong>
+    <>
+      <div className="city_card">
+        <div className="city_card-wrapper">
+          <img src={img} alt="" />
+          <div className="city_card-content">
+            <div className="city_card-title">
+              <h3>{item.title}</h3>
+              <div className="city_card-details">
+                <strong>{item.ratingsAverage}/5</strong>
 
-              <small>
-                {item.ratingsQuantity} reviews | {item.travellers} travellers
-              </small>
-              {item.cancellation ? (
-                <span className="free">Free cancellation</span>
-              ) : (
-                <span className="non">Non refundable</span>
-              )}
+                <small>
+                  {item.ratingsQuantity} reviews | {item.travellers} travellers
+                </small>
+                {item.cancellation ? (
+                  <span className="free">Free cancellation</span>
+                ) : (
+                  <span className="non">Non refundable</span>
+                )}
+              </div>
             </div>
+            <div className="city_card-desc">{item.desc}</div>
           </div>
-          <div className="city_card-desc">{item.desc}</div>
+        </div>
+        <div className="city_card-quality-tags">
+          <div className="city_card-tag">
+            <small>
+              <LanguageIcon />
+              {item.language}
+            </small>
+            <small>
+              <BusIcon />
+              Tour package
+            </small>
+          </div>
+          <div className="city_card-price">
+            <h3>&euro; {item.price}</h3>
+          </div>
         </div>
       </div>
-      <div className="city_card-quality-tags">
-        <div className="city_card-tag">
-          <small>
-            <LanguageIcon />
-            {item.language}
-          </small>
-          <small>
-            <BusIcon />
-            Tour package
-          </small>
-        </div>
-        <div className="city_card-price">
-          <h3>&euro; {item.price}</h3>
-        </div>
-      </div>
-    </div>
+    </>
   )
 }
 
