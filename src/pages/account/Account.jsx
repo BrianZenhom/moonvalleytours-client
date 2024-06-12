eec9022c72b03e78661eb75fe843fa4ebd69f542
brianzenhom <brianzenhom@hotmail.com>
2024-06-12 13:17:52 +0200

    Fix routes

--- src/pages/account/Account.jsx
+++ src/pages/account/Account.jsx
@@ -1,6 +1,18 @@
+import { useContext, useEffect } from 'react'
 import './account.css'
+import { AuthContext } from '../../context/AuthContext'
+import { useNavigate } from 'react-router-dom'
 
 const Account = () => {
+  const { user } = useContext(AuthContext)
+  const navigate = useNavigate()
+
+  useEffect(() => {
+    if (!user) {
+      navigate('/')
+    }
+  })
+
   return <section className="account container"></section>
 }
 
