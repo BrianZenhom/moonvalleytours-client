7429b69fbb0d421790e11a45985d6d2f4eeceea3
brianzenhom <brianzenhom@hotmail.com>
2024-06-27 15:43:41 +0200

    Add sidebar to my account page

---
+++ src/components/accountSidebar/accountSidebar.jsx
@@ -0,0 +1,40 @@
+import { useContext } from 'react'
+import { AuthContext } from '../../context/AuthContext'
+import Finger from '../../assets/icons/Finger'
+import Ticket from '../../assets/icons/Ticket'
+import Heart from '../../assets/icons/Heart'
+
+const AccountSidebar = () => {
+  const { user } = useContext(AuthContext)
+  return (
+    <aside className="account__sidebar">
+      <ul>
+        <li className="account__sidebar--user">
+          <span>Hi</span>
+          <strong>{user?.data?.user?.name}</strong>
+          <a href="/customers/my-account">
+            <Finger />
+            <small>My account</small>
+          </a>
+        </li>
+        <hr />
+        <li className="account__sidebar--items">
+          <a href="/customers/bookings">
+            <Ticket />
+            <span>Bookings</span>
+          </a>
+        </li>
+        <hr />
+        <li className="account__sidebar--items">
+          <a href="/customers/favorites">
+            <Heart />
+            <span>Favorites</span>
+          </a>
+        </li>
+        <li></li>
+      </ul>
+    </aside>
+  )
+}
+
+export default AccountSidebar
