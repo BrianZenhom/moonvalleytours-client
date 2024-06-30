557620f9f04f8a35141df3e76c4b2dd5ddffb7e9
brianzenhom <brianzenhom@hotmail.com>
2024-06-30 23:43:33 +0200

    Fix select

--- src/components/select/Select.jsx
+++ src/components/select/Select.jsx
@@ -1,10 +1,11 @@
-import { useEffect, useState } from 'react'
+import { useEffect, useRef, useState } from 'react'
 import './select.css'
 import PropTypes from 'prop-types'
 
-export function Select({ value, onChange, options, classtype }) {
+export function Select({ value, onChange, options, classtype, title }) {
   const [open, setOpen] = useState(false)
   const [highlightedIndex, setHighlightedIndex] = useState(0)
+  const containerRef = useRef(null)
 
   function clearOptions() {
     onChange(undefined)
@@ -22,13 +23,66 @@ export function Select({ value, onChange, options, classtype }) {
     if (open) setHighlightedIndex(0)
   }, [open])
 
+  useEffect(() => {
+    const handler = e => {
+      if (e.target != containerRef.current) return
+      switch (e.code) {
+        case 'Enter':
+        case 'Space':
+          setOpen(prev => !prev)
+          if (open) selectOption(options[highlightedIndex])
+          break
+        case 'ArrowUp':
+        case 'ArrowDown': {
+          if (!open) {
+            setOpen(true)
+            break
+          }
+
+          const newValue = highlightedIndex + (e.code === 'ArrowDown' ? 1 : -1)
+          if (newValue >= 0 && newValue < options.length) {
+            setHighlightedIndex(newValue)
+          }
+          break
+        }
+        case 'Escape':
+          setOpen(false)
+          break
+      }
+    }
+
+    containerRef.current?.addEventListener('keydown', handler)
+
+    return () => {
+      containerRef.current?.removeEventListener('keydown', handler)
+    }
+  }, [open, highlightedIndex, options])
+
+  useEffect(() => {
+    const handleClickOutside = event => {
+      if (
+        containerRef.current &&
+        !containerRef.current.contains(event.target)
+      ) {
+        setOpen(false)
+      }
+    }
+    window.addEventListener('click', handleClickOutside)
+
+    return () => {
+      window.removeEventListener('click', handleClickOutside)
+    }
+  })
+
   return (
     <div
+      ref={containerRef}
       // onBlur={() => setOpen(false)}
       onClick={() => setOpen(prev => !prev)}
       tabIndex={0}
       className={`select__container ${classtype}`}
     >
+      <span className="select__title">{title}</span>
       <span className="select__value">{value?.label}</span>
       <button
         onClick={e => {
@@ -73,4 +127,5 @@ Select.propTypes = {
     })
   ),
   classtype: PropTypes.string,
+  title: PropTypes.string,
 }
