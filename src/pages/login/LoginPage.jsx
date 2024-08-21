1341efffc51df37e855208efafcd316ef00d5be8
brianzenhom <brianzenhom@hotmail.com>
2024-08-21 16:42:50 +0200

    Add user components and login page

---
+++ src/pages/login/LoginPage.jsx
@@ -0,0 +1,10 @@
+import { NewNavbar } from '../../components/newnavbar/NewNavbar'
+import './loginpage.css'
+
+export default function LoginPage() {
+  return (
+    <section className="login_page">
+      <NewNavbar />
+    </section>
+  )
+}
