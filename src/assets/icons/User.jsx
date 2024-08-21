5721b059428fdb46ed034dbfe8b1d0f363fdc8d5
brianzenhom <brianzenhom@hotmail.com>
2024-08-21 16:41:52 +0200

    Add new nav icons

--- src/assets/icons/User.jsx
+++ src/assets/icons/User.jsx
@@ -1,7 +1,7 @@
-const User = ({ type }) => {
+const User = ({ activeIcon }) => {
   return (
     <svg
-      className={type}
+      className={`iconsvgnav ${activeIcon ? 'active' : ''}`}
       xmlns="http://www.w3.org/2000/svg"
       width="25"
       viewBox="0 0 24 24"
