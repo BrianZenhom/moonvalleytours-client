feee91decc2004ff206f73635590ec5392f701fb
brianzenhom <brianzenhom@hotmail.com>
2024-09-24 08:30:15 +0200

    Add New navbar

--- src/components/button/Button.jsx
+++ src/components/button/Button.jsx
@@ -1,7 +1,12 @@
 import './button.css'
 
-const Button = ({ name }) => {
-  return <button className="main_button">{name}</button>
+const Button = ({ name, ...props }) => {
+  const { handleClick } = props
+  return (
+    <button onClick={handleClick} className="main_button">
+      {name}
+    </button>
+  )
 }
 
 export default Button
