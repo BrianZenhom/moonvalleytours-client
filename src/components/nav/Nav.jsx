00d443e6f66ce12a225d04ca28c1c5f00ecfa64e
brianzenhom <brianzenhom@hotmail.com>
2024-09-24 08:30:23 +0200

    Add New navbar

--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -1,4 +1,4 @@
-import { useRef, useState, useEffect } from 'react'
+import { useRef, useState, useEffect, useContext } from 'react'
 import './nav.css'
 import { LanguageSelector } from '../language/LanguageSelector'
 import UserIcon from '../../assets/icons/User'
@@ -10,12 +10,16 @@ import clsx from 'clsx'
 import { CartDropdown } from '../cartdropdown/CartDropdown'
 import Logo from '../../assets/logos/Logo'
 import { QuestionIcon } from '../../assets/icons/AllIcons'
+import { useLocation, useNavigate } from 'react-router-dom'
+import { AuthContext } from '../../context/AuthContext'
+import axios from 'axios'
 
 export function Nav() {
   const [hovering, setHovering] = useState(null)
   const [popoverLeft, setPopoverLeft] = useState(null)
   const [popoverHeight, setPopoverHeight] = useState(null)
   const [popoverWidth, setPopoverWidth] = useState(null)
+  const [setNub, setLeftNub] = useState(null)
 
   const [activeLanguage, setActiveLanguage] = useState('English')
   const [activeCurrency, setActiveCurrency] = useState('Currency')
@@ -23,8 +27,13 @@ export function Nav() {
   const [scroll, setScroll] = useState(false)
 
   const [navScrolled, setNavScrolled] = useState(null)
+  const [credentials, setCredentials] = useState({
+    email: undefined,
+    password: undefined,
+  })
 
   const refs = useRef([])
+  const navigate = useNavigate()
 
   const handleActive = e => {
     setActiveLanguage(e.target.innerText)
@@ -38,6 +47,37 @@ export function Nav() {
     setHovering(index)
     setPopoverLeft(el.offsetLeft)
     setActiveLink(index)
+  }
+
+  const { user, loading, dispatch } = useContext(AuthContext)
+
+  const handleChange = e => {
+    setCredentials(prev => ({ ...prev, [e.target.id]: e.target.value }))
+  }
+
+  const handleClick = async e => {
+    e.preventDefault()
+    dispatch({ type: 'LOGIN_START' })
+    try {
+      const res = await axios.post(
+        'http://localhost:1234/api/v1/auth/login',
+        credentials,
+        {
+          withCredentials: true,
+        }
+      )
+      setHovering(null)
+      dispatch({ type: 'LOGIN_SUCCESS', payload: res.data })
+      navigate('/')
+    } catch (err) {
+      dispatch({ type: 'LOGIN_FAILURE', payload: err.response.data })
+    }
+  }
+
+  const handleLogout = () => {
+    setCredentials(undefined)
+    navigate('/')
+    dispatch({ type: 'LOGOUT' })
   }
 
   useEffect(() => {
@@ -83,12 +123,31 @@ export function Nav() {
     }
   }, [hovering])
 
+  useEffect(() => {
+    const listLinks = document.querySelectorAll('.nav_link')
+
+    listLinks.forEach(item => {
+      item.addEventListener('mouseenter', ({ target }) => {
+        const { left, width } = target.getBoundingClientRect()
+        const centerNub = left + width / 2
+
+        setLeftNub(centerNub)
+      })
+    })
+  })
+
+  const location = useLocation()
+
   return (
     <div
       className={`newnavbar_content ${scroll ? '' : 'activate'} ${
         navScrolled ? 'bg' : ''
-      }`}
+      } ${location.pathname === '/' ? '' : 'withBg'}`}
     >
+      <div
+        className={`custom_nub ${activeLink ? '' : 'nonub'}`}
+        style={{ left: setNub || undefined }}
+      ></div>
       <nav className={scroll ? 'nav_wrapper active' : 'nav_wrapper'}>
         <div className="logo">
           <Logo type="navlogo" />
@@ -103,34 +162,35 @@ export function Nav() {
           }}
           className={`new_nav-links ${scroll ? 'colored_links' : ''}`}
         >
-          <a
-            onMouseEnter={event => onMouseEnter(0, event.currentTarget)}
-            className={clsx('nav_link', activeLink === 0 && 'active')}
-            href="#"
-          >
-            {activeLanguage}
-          </a>
-          <a
+          <span
             onMouseEnter={event => onMouseEnter(1, event.currentTarget)}
             className={clsx('nav_link', activeLink === 1 && 'active')}
             href="#"
           >
-            {activeCurrency}
-          </a>
-          <a
+            {activeLanguage}
+          </span>
+          <span
             onMouseEnter={event => onMouseEnter(2, event.currentTarget)}
             className={clsx('nav_link', activeLink === 2 && 'active')}
             href="#"
           >
-            <UserIcon activeIcon={activeLink === 2} />
-          </a>
-          <a
+            {activeCurrency}
+          </span>
+          <span
             onMouseEnter={event => onMouseEnter(3, event.currentTarget)}
             className={clsx('nav_link', activeLink === 3 && 'active')}
             href="#"
           >
-            <Cart activeIcon={activeLink === 3} />
-          </a>
+            <UserIcon activeIcon={activeLink === 3} />{' '}
+            {user ? <span>{user.name}</span> : ''}
+          </span>
+          <span
+            onMouseEnter={event => onMouseEnter(4, event.currentTarget)}
+            className={clsx('nav_link', activeLink === 4 && 'active')}
+            href="#"
+          >
+            <Cart activeIcon={activeLink === 4} />
+          </span>
           <div
             style={{
               left: popoverLeft || undefined,
@@ -143,25 +203,39 @@ export function Nav() {
             )}
             id="overlay-content"
           >
-            <SlideWrapper index={0} hovering={hovering}>
+            <SlideWrapper index={1} hovering={hovering}>
               <LanguageSelector
-                ref={element => (refs.current[0] = element)}
+                ref={element => (refs.current[1] = element)}
                 handleActive={handleActive}
               />
             </SlideWrapper>
-            <SlideWrapper index={1} hovering={hovering}>
+            <SlideWrapper index={2} hovering={hovering}>
               <Currency
-                ref={element => (refs.current[1] = element)}
+                ref={element => (refs.current[2] = element)}
                 handleActiveCurr={handleActiveCurr}
               />
             </SlideWrapper>
 
-            <SlideWrapper index={2} hovering={hovering}>
-              <User ref={element => (refs.current[2] = element)} />
-            </SlideWrapper>
-
             <SlideWrapper index={3} hovering={hovering}>
-              <CartDropdown ref={element => (refs.current[3] = element)} />
+              {user ? (
+                <div
+                  ref={element => (refs.current[3] = element)}
+                  className="user_nav"
+                >
+                  <li>herro</li>
+                  <button onClick={handleLogout}>logout</button>
+                </div>
+              ) : (
+                <User
+                  loading={loading}
+                  handleClick={handleClick}
+                  handleChange={handleChange}
+                  ref={element => (refs.current[3] = element)}
+                />
+              )}
+            </SlideWrapper>
+            <SlideWrapper index={4} hovering={hovering}>
+              <CartDropdown ref={element => (refs.current[4] = element)} />
             </SlideWrapper>
           </div>
         </div>
