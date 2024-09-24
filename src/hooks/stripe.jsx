dfcef9f4eadf48b87e3b1ac720d2c1c0f6437072
brianzenhom <brianzenhom@hotmail.com>
2024-09-24 08:30:37 +0200

    Add New navbar

--- src/hooks/stripe.jsx
+++ src/hooks/stripe.jsx
@@ -1,12 +1,19 @@
 import { useContext } from 'react'
 import axios from 'axios'
 import { AuthContext } from './../context/AuthContext' // Import your AuthContext
+import { useNavigate } from 'react-router-dom'
 
 export default function TourBookingComponent({ tourId }) {
+  const navigate = useNavigate()
   const { user, loading, error } = useContext(AuthContext)
 
   const bookTour = async event => {
     event.preventDefault() // Prevent default form action or link navigation
+
+    if (!user) {
+      navigate('/customers/login')
+      return
+    }
 
     if (loading) return // Prevent action if loading
     if (error) {
@@ -15,25 +22,15 @@ export default function TourBookingComponent({ tourId }) {
     }
 
     try {
-      console.log(user)
-      const token = user?.token // Assuming your token is stored in the user object
-
-      if (!token) {
-        console.error('No token found, user may not be authenticated')
-        return
-      }
-
       const session = await axios.post(
         `http://localhost:1234/api/v1/bookings/checkout-session/${tourId}`,
         {}, // Empty data object since you're sending no body content
         {
-          headers: {
-            Authorization: `Bearer ${token}`,
-          },
+          withCredentials: true,
         }
       )
 
-      console.log(session.data) // Log session data from response
+      window.open(`${session.data.redirectURL}`)
     } catch (err) {
       console.error('Error during booking:', err)
     }
