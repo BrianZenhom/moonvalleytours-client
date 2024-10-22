d00745f46d8982315f20aa0645a99f45203619da
brianzenhom <brianzenhom@hotmail.com>
2024-10-22 02:06:33 +0200

    Update link

--- src/components/navbarMobile/navbarMobile.jsx
+++ src/components/navbarMobile/navbarMobile.jsx
@@ -46,7 +46,7 @@ const NavbarMobile = ({
     dispatch({ type: 'LOGIN_START' })
     try {
       const res = await axios.post(
-        'http://13.60.105.136/api/v1/auth/login',
+        'http://api.moonvalleytours.lat/api/v1/auth/login',
         credentials
       )
       dispatch({ type: 'LOGIN_SUCCESS', payload: res.data })
