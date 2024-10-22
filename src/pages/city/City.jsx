cb2cb3ffb896eb3956178cd1b6cccadf7cb07a22
brianzenhom <brianzenhom@hotmail.com>
2024-10-22 02:06:43 +0200

    Update link

--- src/pages/city/City.jsx
+++ src/pages/city/City.jsx
@@ -18,7 +18,7 @@ export default function City() {
   }
 
   const { data, loading, error } = Hooks.useFetch(
-    `http://13.60.105.136/api/v1/cities/${id}`
+    `http://api.moonvalleytours.lat/api/v1/cities/${id}`
   )
 
   const CategoryItems = [
